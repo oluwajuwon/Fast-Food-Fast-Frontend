97e4bb301a7503d473d2b6c5ddad6f2bda95c6a5
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2019-01-10 13:21:04 +0100

    [Chore #162901294] Setup project to use webpack  …  - Setup client side of project to use react  - Setup react project to use webpack  - Update babel dependencies to latest version
    [Finishes #162901294]

---
+++ webpack.config.dev.js
@@ -0,0 +1,43 @@
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['@babel/preset-env', '@babel/react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|JPG)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  target: 'web',
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: `${__dirname}/public`,
+    port: 5000,
+    historyApiFallback: true,
+  },
+};
